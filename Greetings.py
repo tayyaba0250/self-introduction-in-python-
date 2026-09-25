773e6f5382c42210373a6a4819777a46e4337207
tayyaba0250 <tayyaba0250@gmail.com>
2026-09-25 23:45:37 +0530

    Create Greetings.py
    greetings

---
+++ Greetings.py
@@ -0,0 +1,7 @@
+#Greetings 
+
+print("Hello, Python!")
+print("My name is *****.")
+print("I am learning Python.")
+print("My goal is to become a good programmer.")
+print("My hobby is to innovate different projects in python.")
